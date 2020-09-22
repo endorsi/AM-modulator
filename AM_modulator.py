39275bb7f43f2a51dffcfa5a02ba015fc7b65594
endorsi <66705950+endorsi@users.noreply.github.com>
2020-09-22 13:27:12 +0300

    Add files via upload

---
+++ AM_modulator.py
@@ -0,0 +1,68 @@
+# AM modulator
+
+import matplotlib
+
+signal = input("m(t) = ")
+A = float(input("A = "))
+fc = float(input("Frequency of Carrier = "))
+
+if "sin" in signal:
+    mp = signal[:signal.find("s")]
+    if mp.isdecimal():
+        mp=int(mp)
+    else:
+        mp=1
+
+    value = signal.split("(")[1]
+    for i in ["pi",".","*","t",")"]:
+        value = value.replace(i,"")
+
+    value = int(value)
+    mts=[]
+    out=[]
+    time=[]
+    for t in range(10000):
+        t = t / 1000000
+        mt= mp*np.sin(value*math.pi*t)
+        st= (A + mt) * np.cos(2*fc*math.pi*t)
+        mts.append(mt)
+        out.append(st)
+        time.append(t)
+
+if "cos" in signal:
+    mp = signal[:signal.find("c")]
+    if mp.isdecimal():
+        mp=int(mp)
+    else:
+        mp=1
+
+    value = signal.split("(")[1]
+    for i in ["pi",".","*","t",")"]:
+        value = value.replace(i,"")
+
+    value = int(value)
+    mts=[]
+    out=[]
+    time=[]
+    for t in range(10000):
+        t = t / 1000000
+        mt= mp*np.cos(value*math.pi*t)
+        st= (A + mt) * np.cos(2*fc*math.pi*t)
+        mts.append(mt)
+        out.append(st)
+        time.append(t)
+
+u = mp/A
+PowerE = str((u*u) / ( (u*u) + 2 ) * 100)
+PowerE = "Power Efficiency = %" + PowerE[:4]
+upper = np.max(out)-0.5
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+ax1.plot(time, mts, label="s(t)")
+ax1.plot(time, out, label="m(t)")
+ax1.text(0.006, upper, PowerE, style='italic',
+         bbox={'facecolor': 'green', 'alpha': 0.8, 'pad': 6})
+ax1.legend(loc=4)
+ax1.set(xlabel="t")
+plt.show()
